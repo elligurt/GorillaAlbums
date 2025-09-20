ceb6acfb746f75c0c5e9560511b954ae9e27ff45
elliot! <benjikrolfanpageuwu@gmail.com>
2025-09-20 23:03:41 +0100

    Bump Version

--- GorillaAlbums/Constants.cs
+++ GorillaAlbums/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.elliot.gorillatag.gorillaalbums";
         public const string Name = "GorillaAlbums";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.2";
     }
 }
